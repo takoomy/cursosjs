892788c39b57acf5ecc0df6b9ff774ce11747a8a
takoomy <108832443+takoomy@users.noreply.github.com>
2022-11-22 13:56:14 -0300

    Estudando estruturas de repetição

---
+++ aula13/ambiente.js
@@ -0,0 +1,16 @@
+//Estrutura de repetição com teste logico no inicio!
+
+/*var contador = 1 
+while (contador <= 6) {
+    console.log(`Passo ${contador}`);
+    contador++
+}*/
+
+
+//Estrutura de repeticao com teste logocio no fim!
+var contador = 1 
+do {
+    console.log(`Passo ${contador}`);
+    contador++
+} while (contador <= 10)
+
