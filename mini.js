51ce5b568689bb7d19c2c7a937b84aadf37af5a7
takoomy <108832443+takoomy@users.noreply.github.com>
2022-11-25 12:21:30 -0300

    teste pratico
    procurando alunos aprovados

---
+++ mini.js
@@ -0,0 +1,67 @@
+/*const calcular = function(operacao, num1, num2) {
+    return operacao(num1, num2);
+}
+const soma = function(num1,num2) {
+    return num1 + num2;
+}
+const sub = function(num1, num2) {
+    return num1 - num2;
+}
+
+const resultSoma = calcular(soma, 0, 5);
+const resultSub = calcular(sub, 1, 2);
+
+console.log(resultSoma);
+console.log(resultSub);*/
+
+
+/*function exponencial(array, num = 1) {
+    const result = [];
+
+    for (let i = 0; i < array.length; i++) {
+        result.push(array[i] ** num);
+    }
+    return result;
+}
+
+exponencial([1, 2, 3, 4])*/
+
+/*function findMax() {
+    let max = - Infinity;
+
+    for (let i = 0; i < arguments.length; i++) {
+        if (arguments[i] > max) {
+            max = arguments[i];
+        }
+    }
+    return max
+}
+
+console.log(findMax(1, 2, 3, 6, 96, 1))*/
+
+//array com a tecnica rest unsando (...arguments)
+/*function confereTamanho(...arguments) {
+    console.log(arguments.length)
+}
+confereTamanho(1,2)*/
+
+const soma = (a, b) => a + b;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
